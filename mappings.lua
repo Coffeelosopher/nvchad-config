bfff0b285ed97990d44a85de21d110b775ddeabc
Nico Nicklis <nico.nicklis@icloud.com>
2024-12-13 02:50:54 +0100

    repo creation

---
+++ mappings.lua
@@ -0,0 +1,21 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map Lazy to <leader>l
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- map save to leader + f + s
+map("n", "<leader>fs", "<cmd> w <cr>", { desc = "Save" })
+
+-- map leader f p to :cd to nvim config dir and focuses on nvimtree
+map("n", "<leader>fp", "<cmd>cd ~/Appdata/Local/nvim/<cr><cmd>NvimTreeFocus<cr>", { desc = "jump to Config Directory" })
+
+-- unbind f1 nvim help
+map("n", "<F1>", "<nop>", { desc = "unbind F1" })
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
